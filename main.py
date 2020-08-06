9ca0c9627d42ab040216edfdf393878ff10976a8
ambiso <ambiso@invalid>
2020-08-06 21:12:22 +0200

    Fix

--- main.py
+++ main.py
@@ -150,8 +150,10 @@ def ride_trains(strip, wait_ms=10):
                 strip.setPixelColor(i + LEFT, Color(0,0,0))
             else:
                 col = [color_map[floor(hue * (len(color_map)-1))][floor(val * (len(color_map)-1))] for (hue, val) in colors]
-                col = np.sum(col, axis=0) / len(col)
+                col = np.sum(col, axis=0)
                 col = np.array(col, dtype=np.int).tolist()
+                for i in range(len(col)):
+                    col[i] = min(col[0], 255)
                 color = Color(*col)
                 strip.setPixelColor(i + LEFT, color)
         strip.show()
