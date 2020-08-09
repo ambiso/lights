ceca81ee9362b54a84c81526a4a48204d05ef9f1
ambiso <ambiso@invalid>
2020-08-09 21:35:04 +0200

    Fix

--- main.py
+++ main.py
@@ -286,11 +286,15 @@ if __name__ == '__main__':
     app = Flask(__name__)
     current_brightness = 1.
 
-    @app.route('/brightness/<float:brightness>', methods=['POST'])
-    def set_brightness(brightness: float):
+    @app.route('/brightness/<brightness>', methods=['POST'])
+    def set_brightness(brightness):
         global current_brightness
-        current_brightness = brightness
-        return json.dumps({"sucess": True}, separators=(",", ":"))
+        b = float(brightness)
+        if 0. < b < 1.:
+            current_brightness = b
+            return json.dumps({"sucess": True, "brightness": b}, separators=(",", ":"))
+        else:
+            return json.dumps({"sucess": False})
 
     @app.route('/')
     def home():
