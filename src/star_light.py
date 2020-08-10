3ae84647cc2e5bc26afe277dadb8c82716c30614
ambiso <ambiso@invalid>
2020-08-10 22:18:06 +0200

    Make less slow

--- src/star_light.py
+++ src/star_light.py
@@ -57,7 +57,7 @@ def sparkle(strip, get_current_brightness = lambda: 1.):
     sparkles = [] # (pos, time)
 
     n = 1000
-    slowness = 100
+    slowness = 1
     last_brightness = get_current_brightness()
     sparkle_cache = make_sparkle_cache(n, last_brightness)
     t = 0
