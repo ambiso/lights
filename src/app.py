88f66b6906c700becf4c56ca6ca3e85912879d85
Huy <huy@hhoang.dev>
2020-08-11 22:10:45 +0200

    convert to tabs

--- src/app.py
+++ src/app.py
@@ -10,23 +10,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    print(os.getcwd(), file=sys.stderr)
-    return render_template("index.html", animation=main.curr_animation, brightness=main.curr_brightness)
+	return render_template("index.html", animation=main.curr_animation, brightness=main.curr_brightness)
 
 @app.route('/brightness/<brightness>', methods=['POST'])
 def set_brightness(brightness):
-    b = int(brightness)
-    if 0 <= b <= 255:
-        main.curr_brightness = b
-        return json.dumps({"sucess": True, "brightness": b}, separators=(",", ":"))
-    else:
-        return json.dumps({"sucess": False})
+	b = int(brightness)
+	if 0 <= b <= 255:
+		main.curr_brightness = b
+		return json.dumps({"sucess": True, "brightness": b}, separators=(",", ":"))
+	else:
+		return json.dumps({"sucess": False})
 
 
 @app.route('/animation/<animation>', methods=['POST'])
 def set_animation(animation):
-    main.curr_animation = animation
-    return json.dumps({"sucess": True, "animation": animation}, separators=(",", ":"))
+	main.curr_animation = animation
+	return json.dumps({"sucess": True, "animation": animation}, separators=(",", ":"))
 
 def run():
-    app.run(host="0.0.0.0", port=1337)
+	app.run(host="0.0.0.0", port=1337)
