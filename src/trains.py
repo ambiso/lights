9e06041db3ccc0154fe34b3fa68d548af95fc215
ambiso <ambiso@invalid>
2020-08-11 22:36:25 +0200

    Cap at 255

--- src/trains.py
+++ src/trains.py
@@ -58,7 +58,9 @@ def ride_trains(strip, null=None):
         strip.setPixelColor(i + LEFT, Color(0,0,0))
       else:
         col = [color_map[floor(hue * (len(color_map)-1))][floor(val * (len(color_map)-1))] for (hue, val) in colors]
-        col = np.sum(col, axis=0) / len(col)
+        col = np.sum(col, axis=0)
+        for i in range(len(col)):
+          col[i] = min(col[i], 255)
         col = np.array(col, dtype=np.int).tolist()
         color = Color(*col)
         strip.setPixelColor(i + LEFT, color)
