388ff0052778138c5315cb7d8aaec6cee5c779a4
ambiso <ambiso@invalid>
2020-08-11 23:51:46 +0200

    No negative speed

--- src/trains.py
+++ src/trains.py
@@ -38,7 +38,7 @@ def ride_trains(strip):
         c = next(self.color_provider)
         vstrip[i % len(vstrip)].append((c, gauss(i, self.position + self.width/2, self.width / 5)))
       self.speed += random.uniform(-0.01, 0.01)
-      self.speed = min(.9, max(-.9, self.speed))
+      self.speed = min(.9, max(0, self.speed))
       self.width += random.uniform(-0.01, 0.01)
       self.width = min(60, max(10, self.width))
       self.position += self.speed
