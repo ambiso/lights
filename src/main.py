34ca2fb86ff87723fc0ee7aa88c1889f3d08adc8
ambiso <ambiso@invalid>
2020-08-11 23:47:06 +0200

    OVERCLOCKING

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ from src import animations
 # LED strip configuration:
 LED_COUNT      = 300     # Number of LED pixels.
 LED_PIN        = 10      # GPIO pin connected to the pixels (must support PWM!).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_FREQ_HZ    = 1600000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
