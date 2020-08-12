432348e3fce623ef79104e48fdd21e64d0ec573b
Huy <huy@hhoang.dev>
2020-08-12 02:04:45 +0200

    .

--- src/star_light.py
+++ src/star_light.py
@@ -44,7 +44,7 @@ def sparkle(strip):
 	sparkles = [] # (pos, time)
 
 	n = 1000
-	slowness = 100
+	slowness = 10
 	sparkle_cache = make_sparkle_cache(n)
 	t = 0
 	fill(strip, sparkle_cache[t // slowness][-1])
