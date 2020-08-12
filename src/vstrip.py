c2735061fc3a69fb223620f03745c40e88ebe8f7
ambiso <ambiso@invalid>
2020-08-12 22:53:03 +0200

    Fix

---
+++ src/vstrip.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+class VStrip:
+	def __init__(self, strip):
+		self.strip = [(0, 0, 0, 0) for _ in range(strip.numPixels())]
+
+	def setPixelColor(self, i, color):
+		self.strip[i] = color
+
+	def numPixels(self):
+		return len(self.strip)
+
+	def getPixelColor(self, i):
+		return self.strip[i]
+
