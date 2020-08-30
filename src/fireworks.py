ee5f9608ce2ce7d7659cfdf4b168bfc8f95258fe
ambiso <ambiso@invalid>
2020-08-30 19:41:55 +0200

    Cube

--- src/fireworks.py
+++ src/fireworks.py
@@ -5,7 +5,7 @@ def fireworks(strip):
 	while True:
 		for pos in range(strip.numPixels()):
 			r = random.uniform(0., 1.)
-			r = int(r * r * 255)
+			r = int(r * r * r * 255)
 			strip.setPixelColor(pos, Color(r, r, r))
 		strip.show()
 		yield
