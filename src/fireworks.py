867412fce83b59e26b776212d6ef5d982fcb1dfa
ambiso <ambiso@invalid>
2020-08-30 19:43:32 +0200

    Weird scaling

--- src/fireworks.py
+++ src/fireworks.py
@@ -1,11 +1,14 @@
 import random
 from rpi_ws281x import Color
+import math
 
 def fireworks(strip):
 	while True:
 		for pos in range(strip.numPixels()):
-			r = random.uniform(0., 1.)
-			r = int(r * r * r * 255)
+			r = random.uniform(0., 100.)
+			#r = int(r * r * r * 255)
+			r = math.sqrt(r * 100)
+
 			strip.setPixelColor(pos, Color(r, r, r))
 		strip.show()
 		yield
