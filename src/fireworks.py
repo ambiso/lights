ce9c3331b22203432ee43d691a125ed1e0458db8
ambiso <ambiso@invalid>
2020-08-30 20:26:09 +0200

    Paren

--- src/fireworks.py
+++ src/fireworks.py
@@ -6,7 +6,7 @@ def fireworks(strip):
 	t = 0.1
 	while True:
 		for pos in range(strip.numPixels()):
-			v = (math.sin(t) + 1) / 2 / t * 10
+			v = (math.sin(t) + 1) / 2 / (t * 10)
 			v = int(v * 255)
 			strip.setPixelColor(pos, Color(v, v, v))
 		strip.show()
