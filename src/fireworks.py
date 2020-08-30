4f863c71df7142a2fe93c25b854c88361730ff67
ambiso <ambiso@invalid>
2020-08-30 19:40:55 +0200

    Squared brightness

--- src/fireworks.py
+++ src/fireworks.py
@@ -4,7 +4,8 @@ from rpi_ws281x import Color
 def fireworks(strip):
 	while True:
 		for pos in range(strip.numPixels()):
-			r = random.randint(0, 255)
+			r = random.uniform(0., 1.)
+			r = int(r * r * 255)
 			strip.setPixelColor(pos, Color(r, r, r))
 		strip.show()
 		yield
